f94f657356be3a2cb04b66e917e2b0d0deda791a
Daniel Kleinstein <daniel@pelanor.io>
2023-10-28 14:45:31 +0300

    terraform: Add iperf and ping-allowing security groups

--- templates/ec2_instance.tpl.tf
+++ templates/ec2_instance.tpl.tf
@@ -6,12 +6,16 @@ resource "aws_instance" "ec2_instance_REGION_AZ_REPLACE_ME" {
   key_name          = aws_key_pair.development_server_key_pair_REGION_ALIAS_REPLACE_ME.key_name
   vpc_security_group_ids = [
     aws_security_group.allow_ssh_security_group_REGION_ALIAS_REPLACE_ME.id,
-    aws_security_group.allow_all_outbound_traffic_security_group_REGION_ALIAS_REPLACE_ME.id
+    aws_security_group.allow_all_outbound_traffic_security_group_REGION_ALIAS_REPLACE_ME.id,
+    aws_security_group.allow_iperf3_traffic_REGION_ALIAS_REPLACE_ME.id,
+    aws_security_group.allow_ping_traffic_REGION_ALIAS_REPLACE_ME.id
   ]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "${aws_sqs_queue.sqs_queue_REGION_ALIAS_REPLACE_ME.url}" > /sqs-queue
+              sudo apt update -y
+              sudo apt install iperf3 -y
               EOF
 
   root_block_device {
--- templates/security_group.tpl.tf
+++ templates/security_group.tpl.tf
@@ -11,6 +11,32 @@ resource "aws_security_group" "allow_ssh_security_group_REGION_ALIAS_REPLACE_ME"
   }
 }
 
+resource "aws_security_group" "allow_iperf3_traffic_REGION_ALIAS_REPLACE_ME" {
+  name        = "allow_iperf3_traffic_REGION_ALIAS_REPLACE_ME"
+  description = "Allow iperf3 inbound traffic for REGION_ALIAS_REPLACE_ME"
+  provider    = aws.REGION_ALIAS_REPLACE_ME
+
+  ingress {
+    from_port   = 5201
+    to_port     = 5201
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_ping_traffic_REGION_ALIAS_REPLACE_ME" {
+  name        = "allow_ping_traffic_REGION_ALIAS_REPLACE_ME"
+  description = "Allow ping inbound traffic for REGION_ALIAS_REPLACE_ME"
+  provider    = aws.REGION_ALIAS_REPLACE_ME
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "allow_all_outbound_traffic_security_group_REGION_ALIAS_REPLACE_ME" {
   name        = "allow-all-outbound-traffic_REGION_ALIAS_REPLACE_ME"
   description = "Allow all outbound traffic for REGION_ALIAS_REPLACE_ME"
